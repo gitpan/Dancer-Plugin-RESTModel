c04cee1c97704fc8710266c29d3eefc27a78b2a6
Breno G. de Oliveira <garu@cpan.org>
2014-05-24 14:43:31 +0000

    Import of GARU/Dancer-Plugin-RESTModel-0.02 from CPAN.
    gitpan-cpan-distribution: Dancer-Plugin-RESTModel
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         GARU/Dancer-Plugin-RESTModel-0.02.tar.gz
    gitpan-cpan-author:       GARU
    gitpan-cpan-maturity:     released

--- lib/Dancer/Plugin/RESTModel.pm
+++ lib/Dancer/Plugin/RESTModel.pm
@@ -9,7 +9,7 @@ use Dancer qw( :syntax :moose );
 use Dancer::Plugin;
 use Carp 'croak';
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 my $schemas = {};
 
@@ -71,7 +71,7 @@ This plugin lets you talk to a REST server as a separate model from within
 your Dancer app. It is useful for keeping your API decoupled from your app
 while still being able to manage it through the configuration file.
 
-It is a a thin wrapper over L<Role::REST::Client>.
+It is a thin wrapper over L<Role::REST::Client>.
 
 =head1 INTERFACE 
 
@@ -153,7 +153,7 @@ Breno G. de Oliveira  C<< <garu@cpan.org> >>
 
 =head1 LICENCE AND COPYRIGHT
 
-Copyright (c) 2013, Breno G. de Oliveira C<< <garu@cpan.org> >>. All rights reserved.
+Copyright (c) 2013-2014, Breno G. de Oliveira C<< <garu@cpan.org> >>. All rights reserved.
 
 This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself. See L<perlartistic>.
